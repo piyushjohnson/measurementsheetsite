8bc878c89acbb47947e88db4ce99ee88a51e909c
Piyush Johnson <34616118+piyushjohnson@users.noreply.github.com>
2022-04-01 16:53:05 +0530

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	// Set "renderers" to "[]" to disable all default, builtin component support.
+	renderers: [],
+});
